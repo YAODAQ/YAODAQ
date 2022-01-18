94e46a33fbd87af1c0571c7700e7fcc6abc74a63
flagarde <flagarde.programs@pm.me>
2022-01-18 22:28:26 +0800

    fix: Fix test

--- tests/Identifier.test.cpp
+++ tests/Identifier.test.cpp
@@ -11,7 +11,7 @@ TEST_CASE( "Identifier" )
   yaodaq::Identifier id( yaodaq::Class::WebsocketServer, "MyType", "MyName" );
   CHECK_EQ( id.getName(), "MyName" );
   CHECK_EQ( id.getType(), "MyType" );
-  CHECK_EQ( id.getClass(), "WebSocketServer" );
+  CHECK_EQ( id.getClass(), "WebsocketServer" );
   CHECK_EQ( id.getClassId(), yaodaq::Class::WebsocketServer );
   CHECK_EQ( id.get(), "WebsocketServer/MyType/MyName" );
 }
