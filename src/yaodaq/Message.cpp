856869b54d7c3fd958d12ae716257fce7963aabc
flagarde <flagarde.programs@pm.me>
2022-02-15 02:58:41 +0800

    Update Message.cpp

--- src/yaodaq/Message.cpp
+++ src/yaodaq/Message.cpp
@@ -23,11 +23,11 @@ namespace yaodaq
 
 Message::Message()
 {
-  m_JSON["from"];
-  m_JSON["to"];
+  m_JSON["from"] = nlohmann::json{};
+  m_JSON["to"] = nlohmann::json{};
   m_JSON["type"] = magic_enum::enum_name( MessageType::Unknown );
   m_JSON["uuid"] = ix::uuid4();
-  m_JSON["content"];
+  m_JSON["content"] = nlohmann::json{};
   m_JSON["timestamp"] = fmt::format( "{:%F %T %z}", fmt::gmtime( std::chrono::system_clock::to_time_t( std::chrono::system_clock::now() ) ) );
   m_JSON["meta"]["compiler"]                = nlohmann::json::meta()["compiler"];
   m_JSON["meta"]["platform"]                = nlohmann::json::meta()["platform"];
